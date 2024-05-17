55eb59a3b61aaa1b83aa97f8623ad6eb60cdf0ab
Cameron King <35708477+cameronking4@users.noreply.github.com>
2024-05-17 21:46:16 +0000

    Vercel KV database

--- api/webhook/.endOfCallReport.ts
+++ api/webhook/.endOfCallReport.ts
@@ -1,4 +1,5 @@
 import { EndOfCallReportPayload } from "../../types/vapi.types";
+import { kv } from '@vercel/kv';
 
 export const endOfCallReportHandler = async (
   payload?: EndOfCallReportPayload
@@ -7,6 +8,17 @@ export const endOfCallReportHandler = async (
    * Handle Business logic here.
    * You can store the information like summary, typescript, recordingUrl or even the full messages list in the database.
    */
+   // Store the end-of-call report details in Vercel KV
+   const { call, summary, transcript, recordingUrl, messages } = payload;
+   const timestamp = new Date().toISOString()
+   await kv.set(`call_report:${timestamp}`, {
+    call,
+    summary,
+    transcript,
+    recordingUrl,
+    messages,
+    timestamp: timestamp,
+  });
 
   return;
 };
